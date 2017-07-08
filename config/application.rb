53aacb1c7e4c7dfc0452c6304001fca5c2cf5923
yann marquet <ymarquet@gmail.com>
2017-07-08 18:44:11 +0100

    Rubocop corrections

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,14 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -21,6 +21,13 @@ module ReactShopApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.action_controller.default_url_options = {
+      host: ENV['HTTP_HOST'] || 'localhost:3000'
+    }
+
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
