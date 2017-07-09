db94cd21445f1acee89057175199088bc96ceccf
yann marquet <ymarquet@gmail.com>
2017-07-09 15:11:48 +0100

    Shopping basket to retrieve an order's amount

--- app/models/shopping_basket.rb
+++ app/models/shopping_basket.rb
@@ -1,5 +1,34 @@
 class ShoppingBasket
+
+  def initialize(products, delivery_charges)
+    self.delivery_charges = delivery_charges
+    self.product_codes    = []
+    self.catalogue = products.each_with_object({}) do |product, catalogue|
+      catalogue[product.code] = product
+    end
+  end
+
+  def add(product_code)
+    product_codes << product_code
+  end
+
   def total
-    37.85
+    return 0.0 if product_codes.empty?
+    (products_total_amount + delivery_charge_amount) * 0.01
+  end
+
+  private
+  attr_accessor :product_codes, :catalogue, :delivery_charges
+
+  def products_total_amount
+    product_codes.map do |code|
+      catalogue[code].price_in_cents
+    end.reduce(:+) || 0
+  end
+
+  def delivery_charge_amount
+    delivery_charges.detect do |delivery_charge|
+      delivery_charge.price_range.include?(products_total_amount)
+    end&.amount_in_cents || 0
   end
 end
--- spec/models/shopping_basket_spec.rb
+++ spec/models/shopping_basket_spec.rb
@@ -1,12 +1,29 @@
 require "rails_helper"
 
-RSpec.describe ShoppingBasket, :product_setup  do
+RSpec.describe ShoppingBasket, :product_setup, :delivery_charge_setup  do
+  subject { described_class.new(products, delivery_charges) }
 
   describe '#total' do
+    context 'Given one no products' do
+      it { expect(subject.total).to eq(0) }
+    end
+
     context 'Given one pair of socks and a blouse' do
-      it {
-        expect(subject.total).to eq(37.85)
-      }
+      before do
+        subject.add(socks.code)
+        subject.add(blouse.code)
+      end
+
+      it { expect(subject.total).to eq(37.85) }
+    end
+
+    context 'Given two pairs of jeans' do
+      before do
+        subject.add(jeans.code)
+        subject.add(jeans.code)
+      end
+
+      it { expect(subject.total).to eq(54.37) }
     end
   end
 end
