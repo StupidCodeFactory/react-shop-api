5a4d9a75b3e7beed891077ea3ce07ba787b17c78
yann marquet <ymarquet@gmail.com>
2017-07-09 20:51:49 +0100

    Shopping basket

--- app/models/shopping_basket.rb
+++ app/models/shopping_basket.rb
@@ -1,34 +1,46 @@
+require 'securerandom'
+
 class ShoppingBasket
 
-  def initialize(products, delivery_charges)
-    self.delivery_charges = delivery_charges
-    self.product_codes    = []
-    self.catalogue = products.each_with_object({}) do |product, catalogue|
+  def initialize(products, delivery_charges, special_offer_catalogue)
+    self.delivery_charges        = delivery_charges
+    self.special_offer_catalogue = special_offer_catalogue
+    self.basket_items            = {}
+    self.product_catalogue       = products.each_with_object({}) do |product, catalogue|
       catalogue[product.code] = product
     end
   end
 
   def add(product_code)
-    product_codes << product_code
+    product       = product_catalogue[product_code]
+    special_offer = check_for_offer_on(product)
+
+    basket_items[SecureRandom.uuid] = {
+      product:        product,
+      price_in_cents: special_offer.price_for(product)
+    }
   end
 
   def total
-    return 0.0 if product_codes.empty?
-    (products_total_amount + delivery_charge_amount) * 0.01
+    return 0.0 if basket_items.empty?
+    ((products_total_amount + delivery_charge_amount) * 0.01).floor(2)
   end
 
   private
-  attr_accessor :product_codes, :catalogue, :delivery_charges
+  attr_accessor :product_catalogue, :delivery_charges, :special_offer_catalogue, :basket_items
 
   def products_total_amount
-    product_codes.map do |code|
-      catalogue[code].price_in_cents
-    end.reduce(:+) || 0
+    basket_items.values.map {|line_item| line_item[:price_in_cents]}.reduce(:+) || 0
   end
 
   def delivery_charge_amount
     delivery_charges.detect do |delivery_charge|
       delivery_charge.price_range.include?(products_total_amount)
     end&.amount_in_cents || 0
+  end
+
+  def check_for_offer_on(product)
+    return SpecialOffer::Null.new unless special_offer_catalogue.key?(product.code)
+    special_offer_catalogue[product.code]
   end
 end
--- spec/models/shopping_basket_spec.rb
+++ spec/models/shopping_basket_spec.rb
@@ -1,7 +1,16 @@
 require "rails_helper"
 
 RSpec.describe ShoppingBasket, :product_setup, :delivery_charge_setup  do
-  subject { described_class.new(products, delivery_charges) }
+  let(:special_offer_catalogue) do
+    {
+      jeans.code => SpecialOffer.new(
+        -> (products) { products.size == 2 },
+        -> (products) { products[-1].price_in_cents / 2.0 }
+      )
+    }
+  end
+
+  subject { described_class.new(products, delivery_charges, special_offer_catalogue) }
 
   describe '#total' do
     context 'Given one no products' do
@@ -24,6 +33,28 @@ RSpec.describe ShoppingBasket, :product_setup, :delivery_charge_setup  do
       end
 
       it { expect(subject.total).to eq(54.37) }
+    end
+
+    context 'Given a pairs of jeans and a blouse' do
+      before do
+        subject.add(jeans.code)
+        subject.add(blouse.code)
+      end
+
+      it { expect(subject.total).to eq(60.85) }
+
+    end
+
+    context 'Given to pairs of socks and 3 pairs of jeans' do
+      before do
+        subject.add(socks.code)
+        subject.add(socks.code)
+        subject.add(jeans.code)
+        subject.add(jeans.code)
+        subject.add(jeans.code)
+      end
+
+      it { expect(subject.total).to eq(98.27) }
     end
   end
 end
